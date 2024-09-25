73f095e2b60519a2ae0a0ed4d80fe117c773f253
Tom Myruski <thomas.myruski@gmail.com>
2024-09-25 15:34:37 -0400

    Removed old code

--- resources/js/block-editor.js
+++ resources/js/block-editor.js
@@ -1,5 +1,9 @@
 wp.domReady(() => {
-    console.log('LOADED MY OWN SCRIPTTTTT');
+
+    // This script monitors the document for changes to the data-style attribute
+    // There is a hidden div that is a sibling of innerblocks and this will
+    // transfer the style tag, so although we cannot add style to the innerblocks,
+    // via php template, we can do it this weird way. 
 
 
     const observer = new MutationObserver(mutations => {
@@ -30,109 +34,4 @@ wp.domReady(() => {
       
       // Observe for both childList and attribute mutations
       observer.observe(document.body, { childList: true, subtree: true, attributes: true });
-
-
-
-
-
-    //   const observer = new MutationObserver(mutations => {
-    //     mutations.forEach(mutation => {
-    //       if (mutation.type === 'childList') {
-    //         mutation.addedNodes.forEach(node => {
-    //           if (node.nodeType === Node.ELEMENT_NODE && node.hasAttribute('data-style')) {
-    //             const styleElement = node.nextElementSibling;
-    //             if (styleElement) {
-    //               styleElement.style.cssText = node.getAttribute('data-style');
-    //               console.log('changed style');
-    //             }
-    //           }
-    //         });
-    //       }
-    //     });
-    //   });
-      
-    //   observer.observe(document.body, { childList: true, subtree: true });
-
-
-
-
-// const checkForElement = () => {
-//     const elementToWatch = document.querySelector('.nkg-group-hidden'); 
-
-//     if (elementToWatch) {
-//         console.log('elementToWatch found:', elementToWatch);
-//         const styleElement = elementToWatch.nextElementSibling;
-//         if (styleElement) { 
-
-            
-//                     styleElement.style.cssText = elementToWatch.getAttribute('data-style');
-//                     console.log('changed style');
-            
-               
-            
-            
-            
-//             const observer = new MutationObserver(mutations => {
-//               mutations.forEach(mutation => {
-//                 if (mutation.type === 'attributes' && mutation.attributeName   
-//              === 'data-style') {
-//                   styleElement.style.cssText = elementToWatch.getAttribute('data-style');
-//                   console.log('changed style');
-//                 }
-//               });
-//             });
-            
-//             observer.observe(elementToWatch, { attributes: true });
-            
-            
-            
-//                     // Remove the event listener to prevent multiple executions
-//                     clearInterval(interval);
-
-//         }
-//     } else {
-//         console.log('elementToWatch not found yet');
-//     }
-// };
-
-// // Poll for the element every 100 milliseconds
-// const interval = setInterval(checkForElement, 100);
-
-
-
-// const checkForElement = () => {
-
-//     const elementToWatchList = document.querySelectorAll('.nkg-group-hidden');
-// if (elementToWatchList) {
-    
-//     elementToWatchList.forEach(elementToWatch => {
-//         const styleElement = elementToWatch.nextElementSibling;
-      
-//         if (styleElement) {
-//           const observer = new MutationObserver(mutations => {
-//             mutations.forEach(mutation => {
-//               if (mutation.type === 'attributes' && mutation.attributeName   
-//        === 'data-style') {
-//                 styleElement.style.cssText = elementToWatch.getAttribute('data-style');
-//                 console.log('changed style');
-//               }
-//             });
-//           });
-      
-//           observer.observe(elementToWatch, { attributes: true });
-//         } else {
-//           console.log('styleElement not found for:', elementToWatch);
-//         }
-//       });
-// } else {
-//     console.log('elementToWatch not found yet');
-// }
-
-
-
-// }
-
-// const interval = setInterval(checkForElement, 100);
-
-
 })
